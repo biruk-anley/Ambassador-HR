5395d3caa47b2dbc1db84c0b0d1eb72a04aa9fa2
Biruk Anley <birukanley2022@gmail.com>
2023-02-11 11:57:17 +0300

    update login.js

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,7 +1,135 @@
 import { useState } from "react";
+import {
+	makeStyles,
+  } from "@material-ui/core";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
+
+
+const useStyles = makeStyles((theme) => ({ 	
+    container: {
+        display:'flex',
+        justifyContent:'center',
+        
+        width: "100%",
+        paddingLeft: "6px",
+        paddingRight: "6px",
+        [theme.breakpoints.down("sm")]: {
+          paddingLeft: 5,
+          paddingRight: 5,
+        },
+      },
+      root: {
+        display: "flex",
+        width:'80%',
+        border: "1px solid rgba(0, 0, 0, .2)",
+        justifyContent:'space-around',
+        flexWrap: "nowrap",
+        background: "white",
+        borderRadius: "15px",
+        height: "550px",
+        padding: 10,
+        "& a": {
+          color: "#3A6351",
+        },
+    
+        [theme.breakpoints.down("sm")]: {
+          "& form": {
+            padding: 0,
+          },
+        },
+      },
+      avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+      },
+      form: {
+        marginLeft:'158px',
+        width: "90%",
+        marginTop:'40px',
+        [theme.breakpoints.down("sm")]: {
+          /*width:'80%'*/
+        },
+        "@media (max-width:960px)": {
+          marginLeft:'105px',
+          marginTop:'10px'
+        },
+      },
+  submit: {
+    background: "#3293A8",
+    borderRadius: "5px",
+    width: "70%",
+    height: "50px",
+    
+    margin: theme.spacing(1, 0, 2),
+    "&:hover": {
+      background: "rgba(50, 147, 168,0.79)",
+    },
+    "@media (max-width:760px)": {
+      width:"80%"
+    },
+  },
+
+  login:{
+    fontWeight:'800',
+    marginLeft:'65px',
+    "@media (max-width:760px)": {
+        
+        marginLeft:'15px'
+      },
+
+  },
+  textField: {
+    margin: "10px 0",
+    borderRadius: "10px",
+    width: "70%",
+
+   
+    borderTopLeftRadius: "10px",
+    borderBottomLeftRadius: "10px",
+    border: "0px solid #eee",
+    borderLeftWidth: "7px",
+    borderLeftColor: "rgba(215,215,215,0.47)",
+    "& input": {
+      color: "rgba(57,50,50,0.25)",
+      border: "0px solid #eee",
+      height:'25px',
+      borderRadius: "10px",
+      width: "100%",
+    },
+    "@media (max-width:760px)": {
+      width: "80%",
+    },
+  },
+  texts:
+  {marginLeft:'-210px',
+  "@media (max-width:760px)": {
+    marginLeft:'-105px',
+  }},
+  inputAdornment: {
+    background: "rgba(215,215,215,0.87)",
+    borderRadius: "7px 0px 0px 7px",
+  },
+  imgHolder: {
+  
+    backgroundColor: "rgba(215,215,215,0.1)",
+    marginLeft:'25px',
+    borderRadius: "15px",
+    marginBottom: "auto",
+    display: "flex",
+    width:"70%",
+    marginTop:'auto',
+    height: "auto",
+    paddingBottom: "50px",
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+    width:'40%',
+  },
+}));
+
+
 
 const Login = () => {
 	const [data, setData] = useState({ email: "", password: "" });
@@ -10,6 +138,7 @@ const Login = () => {
 	const handleChange = ({ currentTarget: input }) => {
 		setData({ ...data, [input.name]: input.value });
 	};
+	const classes = useStyles();
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
@@ -32,9 +161,13 @@ const Login = () => {
 	return (
 		<div className={styles.login_container}>
 			<div className={styles.login_form_container}>
+				<div className={styles.right}>
+					
+					
+				</div>
 				<div className={styles.left}>
 					<form className={styles.form_container} onSubmit={handleSubmit}>
-						<h1>Login to Your Account</h1>
+						<h1>Login</h1>
 						<input
 							type="email"
 							placeholder="Email"
@@ -57,16 +190,19 @@ const Login = () => {
 						<button type="submit" className={styles.green_btn}>
 							Sign In
 						</button>
+
+						<div className={styles.otheroption}>
+							<p>Don't you have an Account</p>
+							<Link to="/signup">
+						<a style={{paddingInline:'15px', color:'black', fontWeight:'bold'}}>
+							Sign  Up
+						</a>
+					</Link>
+						</div>
+						
 					</form>
 				</div>
-				<div className={styles.right}>
-					<h1>New Here ?</h1>
-					<Link to="/signup">
-						<button type="button" className={styles.white_btn}>
-							Sing Up
-						</button>
-					</Link>
-				</div>
+				
 			</div>
 		</div>
 	);
